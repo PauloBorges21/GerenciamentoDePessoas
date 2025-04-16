1476bf3c117536500b5c467235e2ae7935ec7a0c
Paulo Silva Borges <paulosbzl@gmail.com>
2025-04-16 06:45:12 -0300

    Melhora na interface de detalhes e exclusão de pessoas
    Adiciona novos links para detalhes e exclusão no arquivo `Index.cshtml`, incluindo modais para exibir informações detalhadas e confirmar a exclusão. Atualiza ícones e aprimora a estrutura dos modais.
    
    Atualiza a versão informativa do assembly em `GerenciamentoDePessoas.AssemblyInfo.cs` e modifica o arquivo `GerenciamentoDePessoas.AssemblyInfoInputs.cache`. Alterações nos arquivos binários indicam mudanças no código compilado.

--- GerenciamentoDePessoas/obj/Debug/net8.0/GerenciamentoDePessoas.AssemblyInfo.cs
+++ GerenciamentoDePessoas/obj/Debug/net8.0/GerenciamentoDePessoas.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GerenciamentoDePessoas")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+394cc65d1c3595bbae86c4962e1385fecd29ba5b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+de82f7be79a15ac688f5f34a0a277e89cce07272")]
 [assembly: System.Reflection.AssemblyProductAttribute("GerenciamentoDePessoas")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GerenciamentoDePessoas")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
